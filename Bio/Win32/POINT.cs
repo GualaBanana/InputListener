6cff0be5ccb2a4ab52ba46e77f23da49d98975b9
Scott <scott@op12.com>
2024-08-05 11:43:18 -0500

    Added MouseHook feature and associated support classes. Added ToString() overrides on certian classes to aide in debugging and logging.

---
+++ Bio/Win32/POINT.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Bio.Win32;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct POINT
+{
+    public int x;
+    public int y;
+
+    public override string ToString()
+    {
+        return $"X:{x} Y:{y}";
+    }
+}
